15fb7ad11c87433ebdea1fd6368ff905a8c8dee7
Arraj2611 <arrajeevaken2611@gmail.com>
2024-02-27 22:39:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Message sent!'); // You can replace this with actual form submission logic
+});
